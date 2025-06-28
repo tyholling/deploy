f7eeaef882cdc2dffe12e7553c98e393fa9a3dfd
Tyler Hollingsworth <tyholling@gmail.com>
2025-06-27 20:26:54 -0700

    configure grafana for mariadb persistence

--- main.tf
+++ main.tf
@@ -84,15 +84,15 @@ resource "kubernetes_namespace" "ingress" {
   }
 }
 
-resource "helm_release" "ingress-nginx" {
-  name       = "ingress-nginx"
+resource "helm_release" "ingress" {
+  name       = "ingress"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
   namespace  = kubernetes_namespace.ingress.metadata[0].name
   values     = [file("${path.module}/ingress-nginx-values.yaml")]
 
   depends_on = [
-    helm_release.cert-manager,
+    helm_release.certmgr,
     helm_release.metallb,
     kubernetes_namespace.ingress,
   ]
@@ -104,8 +104,8 @@ resource "kubernetes_namespace" "metrics" {
   }
 }
 
-resource "helm_release" "metrics-server" {
-  name       = "metrics-server"
+resource "helm_release" "metrics" {
+  name       = "metrics"
   repository = "https://kubernetes-sigs.github.io/metrics-server"
   chart      = "metrics-server"
   namespace  = kubernetes_namespace.metrics.metadata[0].name
@@ -123,8 +123,8 @@ resource "kubernetes_namespace" "localpv" {
   }
 }
 
-resource "helm_release" "localpv-provisioner" {
-  name       = "localpv-provisioner"
+resource "helm_release" "localpv" {
+  name       = "localpv"
   repository = "https://openebs.github.io/dynamic-localpv-provisioner"
   chart      = "localpv-provisioner"
   namespace  = kubernetes_namespace.localpv.metadata[0].name
@@ -142,8 +142,8 @@ resource "kubernetes_namespace" "certmgr" {
   }
 }
 
-resource "helm_release" "cert-manager" {
-  name       = "cert-manager"
+resource "helm_release" "certmgr" {
+  name       = "certmgr"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
   namespace  = kubernetes_namespace.certmgr.metadata[0].name
@@ -158,14 +158,14 @@ resource "helm_release" "cert-manager" {
 resource "kubectl_manifest" "cert-manager-self-signed-issuer" {
   yaml_body = file("cert-manager-self-signed-issuer.yaml")
 
-  depends_on = [helm_release.cert-manager]
+  depends_on = [helm_release.certmgr]
 }
 
 resource "kubectl_manifest" "cert-manager-ca-cert" {
   yaml_body = file("cert-manager-ca-cert.yaml")
 
   depends_on = [
-    helm_release.cert-manager,
+    helm_release.certmgr,
     kubectl_manifest.cert-manager-self-signed-issuer,
   ]
 }
@@ -174,7 +174,7 @@ resource "kubectl_manifest" "cert-manager-cluster-issuer" {
   yaml_body = file("cert-manager-cluster-issuer.yaml")
 
   depends_on = [
-    helm_release.cert-manager,
+    helm_release.certmgr,
     kubectl_manifest.cert-manager-ca-cert,
   ]
 }
@@ -193,8 +193,8 @@ resource "helm_release" "grafana" {
   values     = [file("${path.module}/grafana-values.yaml")]
 
   depends_on = [
-    helm_release.ingress-nginx,
-    helm_release.localpv-provisioner,
+    helm_release.ingress,
+    helm_release.mariadb,
     kubernetes_namespace.grafana,
   ]
 }
@@ -205,7 +205,7 @@ resource "kubernetes_namespace" "mariadb" {
   }
 }
 
-resource "random_string" "mariadb-password" {
+resource "random_password" "mariadb-password" {
   length  = 64
   special = false
   upper   = true
@@ -213,14 +213,25 @@ resource "random_string" "mariadb-password" {
   numeric = true
 }
 
-resource "kubernetes_secret" "mariadb-credentials" {
+resource "kubernetes_secret" "mariadb-credentials-mariadb" {
   metadata {
     name      = "mariadb-credentials"
     namespace = kubernetes_namespace.mariadb.metadata[0].name
   }
   type = "Opaque"
   data = {
-    "mariadb-root-password" = random_string.mariadb-password.result
+    "mariadb-root-password" = random_password.mariadb-password.result
+  }
+}
+
+resource "kubernetes_secret" "mariadb-credentials-grafana" {
+  metadata {
+    name      = "mariadb-credentials"
+    namespace = kubernetes_namespace.grafana.metadata[0].name
+  }
+  type = "Opaque"
+  data = {
+    "mariadb-root-password" = random_password.mariadb-password.result
   }
 }
 
@@ -232,11 +243,12 @@ resource "helm_release" "mariadb" {
   values     = [file("${path.module}/mariadb-values.yaml")]
   set {
     name  = "auth.existingSecret"
-    value = kubernetes_secret.mariadb-credentials.metadata[0].name
+    value = kubernetes_secret.mariadb-credentials-mariadb.metadata[0].name
   }
 
   depends_on = [
     helm_release.flannel,
+    helm_release.localpv,
     kubernetes_namespace.mariadb,
   ]
 }
